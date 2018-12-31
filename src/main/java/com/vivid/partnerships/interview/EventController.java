d0ab4e0cdd692953d69da985c7e7abb6d7c8b111
sebastian.moreno <sebastian.moreno@globant.com>
2018-12-31 17:55:43 -0500

    Base insert event API method

--- src/main/java/com/vivid/partnerships/interview/EventController.java
+++ src/main/java/com/vivid/partnerships/interview/EventController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,5 +26,20 @@ public class EventController {
         List<Event> events = eventService.getEvents();
         LOGGER.info("Returning {} events", events.size());
         return events;
+    }
+
+    @GetMapping("/insertEvent")
+    public boolean insertEvent() {
+        Venue venue = new Venue();
+        venue.name = "RED BULL STADIUM";
+        venue.city = "New York";
+        venue.state = "NY";
+
+        Event event = new Event();
+        event.name = "MSI International";
+        event.date = new Date();
+        event.venue = venue;
+
+        return eventService.insertEvent(event) > 0;
     }
 }
--- src/main/java/com/vivid/partnerships/interview/EventService.java
+++ src/main/java/com/vivid/partnerships/interview/EventService.java
@@ -2,8 +2,15 @@ package com.vivid.partnerships.interview;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 @Service
@@ -16,9 +23,29 @@ public class EventService {
     }
 
      public List<Event> getEvents() {
-         return jdbcTemplate.query("SELECT e.id, e.date, e.name, v.id as venue_id, v.name as venue_name," +
-                 "v.city as venue_city, v.state as venue_state " +
-                 "FROM events e INNER JOIN venues v " +
-                 "WHERE e.venue_id = v.id", new EventRowMapper());
+         return jdbcTemplate.query(
+             "SELECT e.id, e.date, e.name, v.id as venue_id, v.name as venue_name," +
+             "v.city as venue_city, v.state as venue_state " +
+             "FROM events e INNER JOIN venues v " +
+             "WHERE e.venue_id = v.id", new EventRowMapper());
+    }
+
+    public int insertEvent(Event event) {
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        final String insertVenue = "INSERT INTO venues(name, city, state) VALUES (?,?,?)";
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                final PreparedStatement ps = con.prepareStatement(insertVenue, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, event.venue.name);
+                ps.setString(2, event.venue.city);
+                ps.setString(3, event.venue.state);
+                return ps;
+            }
+        }, keyHolder);
+
+        return jdbcTemplate.update("INSERT INTO events(venue_id, name, date) VALUES (?,?,?)",
+            keyHolder.getKey(), event.name, event.date);
     }
 }
